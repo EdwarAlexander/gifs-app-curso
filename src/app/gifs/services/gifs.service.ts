c8b6d482f6cb2f18b0c0cca6627263e0a5e7083a
edwar moran <edwar.moran.12@gmail.com>
2023-11-04 10:29:46 -0500

    chore: finalizacion de pagina

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private apiKey: string = 'rXIGs2QmHYlN8cqRqEKsN8i1aIsB5LBF';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+    this.loadLocalStorage();
+   }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -39,5 +41,17 @@ export class GifsService {
     }
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 }
